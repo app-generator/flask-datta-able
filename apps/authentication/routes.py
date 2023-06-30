7d3415d8d202473aac69762c5c3f79a60e298c29
Aditya12yadav <ay5503352@gmail.com>
2023-06-30 16:53:47 +0530

    Changes made to record the 10 voice samples
    Signed-off-by: Aditya12yadav <ay5503352@gmail.com>

--- apps/authentication/routes.py
+++ apps/authentication/routes.py
@@ -12,11 +12,26 @@ from apps.authentication import blueprint
 from apps.authentication.forms import LoginForm, CreateAccountForm
 from apps.authentication.models import Users
 
-import sounddevice as sd
-from scipy.io.wavfile import write
-import wavio as wv
-
 from apps.authentication.util import verify_pass
+import sounddevice as sd
+import soundfile as sf
+import numpy as np
+
+sample_rate = 44100 
+duration = 10
+num_of_samples = 1
+
+sentences = ["Machine learning 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20",
+             "Birds can be found in every Habitat, from the tundra to the arid desert, and come in a dizzying array of shapes, sizes, and colours",
+             "For many people, birds are simply beings that exist outside their windows or that they might see on a nature hike",
+             "Bird watching can be a relaxing hobby or a lifelong passion, and it offers a unique opportunity to connect with nature",
+             "Each time you go bird watching, you have the chance to encounter new species and learn more about the incredible world we live in",
+             "When you think about recycling, you might simply think about throwing your soda cans and plastic bottles into a blue bin",
+             "However, recycling is much more complicated than that – and it’s important to understand how the process works to do it effectively",
+             "Most recycling plants require that different types of plastics be separated before they can be recycled",
+             "This is because different types of plastics are made of different chemicals, and mixing them can contaminate the batch",
+             "Well, for one thing, it takes a lot less energy to recycle a plastic bottle than it does to create a new one from scratch",
+             "Additionally, recycling helps to reduce the amount of plastic waste that ends up in our landfills and oceans"]
 
 
 @blueprint.route("/")
@@ -121,9 +136,24 @@ def register():
 
 @blueprint.route("/record_voice", methods=["GET", "POST"])
 def record():
-    return render_template(
-        "accounts/record_voice.html",
-    )
+    return render_template("accounts/record_voice.html", text = sentences[0])
+
+@blueprint.route("/record_samples", methods=["POST"])
+def record_samples():
+    global num_of_samples
+    audio = sd.rec(int(sample_rate*duration), samplerate=sample_rate, channels=1) 
+    sd.wait()
+    file_path = f"samples/{num_of_samples}.flac"
+    sf.write(file_path, audio, sample_rate)
+    num_of_samples += 1
+    print(num_of_samples)
+    if(num_of_samples<=10):
+        return render_template('accounts/record_voice.html', text = sentences[0])
+    elif(num_of_samples<21):
+        return render_template('accounts/record_voice.html', text = sentences[num_of_samples-10])
+    else:
+        login_form = LoginForm(request.form)
+        return render_template('accounts/login.html', form=login_form)
 
 
 @blueprint.route("/logout")
