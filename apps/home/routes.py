23cce94fe845fb189ff234a9405266028801bbcf
mominur-helios <mominur.rahman@heliosinsider.com>
2025-02-26 22:34:43 +0600

    Add dynamic profile fields

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -4,6 +4,7 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 import os, json, pprint
+import wtforms
 
 from apps.home import blueprint
 from flask import render_template, request, redirect, url_for
@@ -13,6 +14,8 @@ from flask_login import login_required, current_user
 from apps import db, config
 from apps.models import *
 from apps.tasks import *
+from apps.authentication.models import Users
+from flask_wtf import FlaskForm
 
 @blueprint.route('/')
 @blueprint.route('/index')
@@ -35,28 +38,68 @@ def sample_page():
 def typography():
     return render_template('pages/typography.html', segment='typography')
 
+
+def getField(column): 
+    if isinstance(column.type, db.Text):
+        return wtforms.TextAreaField(column.name.title())
+    if isinstance(column.type, db.String):
+        return wtforms.StringField(column.name.title())
+    if isinstance(column.type, db.Boolean):
+        return wtforms.BooleanField(column.name.title())
+    if isinstance(column.type, db.Integer):
+        return wtforms.IntegerField(column.name.title())
+    if isinstance(column.type, db.Float):
+        return wtforms.DecimalField(column.name.title())
+    if isinstance(column.type, db.LargeBinary):
+        return wtforms.HiddenField(column.name.title())
+    return wtforms.StringField(column.name.title()) 
+
+
 @blueprint.route('/profile', methods=['GET', 'POST'])
 @login_required
 def profile():
-    if request.method == 'POST':
-        first_name = request.form.get('first_name')
-        last_name = request.form.get('last_name')
-        address = request.form.get('address')
-        bio = request.form.get('bio')
+    class ProfileForm(FlaskForm):
+        pass
 
-        current_user.first_name = first_name
-        current_user.last_name = last_name
-        current_user.address = address
-        current_user.bio = bio
+    readonly_fields = Users.readonly_fields
+    full_width_fields = {"bio", "address"}
 
-        try:
-            db.session.commit()
-        except Exception as e:
-            db.session.rollback()
+    for column in Users.__table__.columns:
+        if column.name == "id":
+            continue
 
+        field_name = column.name
+        if field_name in full_width_fields:
+            continue
+
+        field = getField(column)
+        setattr(ProfileForm, field_name, field)
+
+    for field_name in full_width_fields:
+        if field_name in Users.__table__.columns:
+            column = Users.__table__.columns[field_name]
+            field = getField(column)
+            setattr(ProfileForm, field_name, field)
+
+    form = ProfileForm(obj=current_user)
+
+    if form.validate_on_submit():
+        readonly_fields.append("password")
+        excluded_fields = readonly_fields
+        for field_name, field_value in form.data.items():
+            if field_name not in excluded_fields:
+                setattr(current_user, field_name, field_value)
+
+        db.session.commit()
         return redirect(url_for('home_blueprint.profile'))
-
-    return render_template('pages/profile.html', segment='profile')
+    
+    context = {
+        'segment': 'profile',
+        'form': form,
+        'readonly_fields': readonly_fields,
+        'full_width_fields': full_width_fields,
+    }
+    return render_template('pages/profile.html', **context)
 
 
 # Helper - Extract current page name from request
@@ -108,3 +151,10 @@ def tasks_test():
     task = celery_test.delay( input_json )
 
     return f"TASK_ID: {task.id}, output: { task.get() }"
+
+
+# Custom template filter
+
+@blueprint.app_template_filter("replace_value")
+def replace_value(value, arg):
+    return value.replace(arg, " ").title()
